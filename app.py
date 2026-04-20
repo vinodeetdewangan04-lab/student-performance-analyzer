41735f9033bb0654169ca9787702e4a67fb41521
vinodeetdewangan04-lab <vinodeet.dewangan04@gmail.com>
2026-04-20 15:57:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,24 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# ---------------- LOGIN SYSTEM ----------------
-def login():
-    st.title("🔐 Login")
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
 
-    if st.button("Login"):
-        if username == "admin" and password == "1234":
-            st.session_state["logged_in"] = True
-        else:
-            st.error("Invalid credentials")
-
-if "logged_in" not in st.session_state:
-    st.session_state["logged_in"] = False
-
-if not st.session_state["logged_in"]:
-    login()
-    st.stop()
 
 # ---------------- UI CONFIG ----------------
 st.set_page_config(page_title="Student Dashboard", layout="wide")
